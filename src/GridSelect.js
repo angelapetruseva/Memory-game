9291f562b4328697d0efe4255bb45d9476b40f9b
angelapetruseva2 <angela.petruseva2@hotmail.com>
2023-02-03 14:25:23 +0100

    bug fixes

--- src/GridSelect.js
+++ src/GridSelect.js
@@ -12,17 +12,17 @@ Memory.GridSelect.prototype = {
 		var text = this.add.image(500, 5, 'selectText');
 		text.anchor.set(0.5, 0);
 		
-		var size1Btn = this.add.button(90, 110, 'sizeButtons', function() {this.setGridSize(4, 3)}, this);
+		var size1Btn = this.add.button(90, 110, 'sizeButtons', function() {this.setGridSize(3, 3)}, this);
 		size1Btn.loadTexture('sizeButtons', 0);
 		var size2Btn = this.add.button(510, 110, 'sizeButtons', function() {this.setGridSize(4, 4)}, this);
 		size2Btn.loadTexture('sizeButtons', 1);
-		var size3Btn = this.add.button(90, 230, 'sizeButtons', function() {this.setGridSize(5, 4)}, this);
+		var size3Btn = this.add.button(90, 230, 'sizeButtons', function() {this.setGridSize(5, 5)}, this);
 		size3Btn.loadTexture('sizeButtons', 2);
-		var size4Btn = this.add.button(510, 230, 'sizeButtons', function() {this.setGridSize(6, 4)}, this);
+		var size4Btn = this.add.button(510, 230, 'sizeButtons', function() {this.setGridSize(6, 6)}, this);
 		size4Btn.loadTexture('sizeButtons', 3);
-		var size5Btn = this.add.button(90, 350, 'sizeButtons', function() {this.setGridSize(7, 4)}, this);
+		var size5Btn = this.add.button(90, 350, 'sizeButtons', function() {this.setGridSize(7, 7)}, this);
 		size5Btn.loadTexture('sizeButtons', 4);
-		var size6Btn = this.add.button(510, 350, 'sizeButtons', function() {this.setGridSize(8, 4)}, this);
+		var size6Btn = this.add.button(510, 350, 'sizeButtons', function() {this.setGridSize(8, 8)}, this);
 		size6Btn.loadTexture('sizeButtons', 5);
 	},
 	
